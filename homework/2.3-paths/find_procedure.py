6d521c8a5427b0d2bb785fac8c885612285e8c2a
polygalova <polygalova@yandex-team.ru>
2018-01-17 11:01:03 +0300

    add search by sql

--- homework/2.3-paths/find_procedure.py
+++ homework/2.3-paths/find_procedure.py
@@ -1,45 +1,37 @@
-# Задание
-# мне нужно отыскать файл среди десятков других
-# я знаю некоторые части этого файла (на память или из другого источника)
-# я ищу только среди .sql файлов
-# 1. программа ожидает строку, которую будет искать (input())
-# после того, как строка введена, программа ищет её во всех файлах
-# выводит список найденных файлов построчно
-# выводит количество найденных файлов
-# 2. снова ожидает ввод
-# поиск происходит только среди найденных на этапе 1
-# 3. снова ожидает ввод
-# ...
-# Выход из программы программировать не нужно.
-# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
-
-# Пример на настоящих данных
-
-# python3 find_procedure.py
-# Введите строку: INSERT
-# ... большой список файлов ...
-# Всего: 301
-# Введите строку: APPLICATION_SETUP
-# ... большой список файлов ...
-# Всего: 26
-# Введите строку: A400M
-# ... большой список файлов ...
-# Всего: 17
-# Введите строку: 0.0
-# Migrations/000_PSE_Application_setup.sql
-# Migrations/100_1-32_PSE_Application_setup.sql
-# Всего: 2
-# Введите строку: 2.0
-# Migrations/000_PSE_Application_setup.sql
-# Всего: 1
-
-# не забываем организовывать собственный код в функции
-
 import os
 
-migrations = 'Migrations'
-current_dir = os.path.dirname(os.path.abspath(__file__))
 
-if __name__ == '__main__':
-    # ваша логика
-    pass
+def get_sql(dir_for_search):
+    found_files = set()
+    for root, dirs, files in os.walk(dir_for_search):
+        for filename in files:
+            if filename[-4:] == '.sql':
+                found_files.add(filename)
+    return found_files, len(found_files)
+
+
+def search(files, dir_for_search):
+    found_files = set()
+    string_for_search = input('Введите строку: ').lower()
+    for filename in files:
+        with open(os.path.join(dir_for_search, filename), 'r') as f:
+            if string_for_search in f.read().lower():
+                found_files.add(filename)
+    for name in found_files:
+        print(os.path.join(dir_for_search, name))
+    count = len(found_files)
+    print('Всего:', count)
+    return found_files, count
+
+
+def find_sql_by_string():
+    migrations = 'Migrations/'
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    dir_for_search = os.path.dirname(os.path.join(current_dir, migrations))
+    if __name__ == '__main__':
+        found_files, count = get_sql(dir_for_search)
+        while count > 1:
+            found_files, count = search(found_files, dir_for_search)
+
+
+find_sql_by_string()
