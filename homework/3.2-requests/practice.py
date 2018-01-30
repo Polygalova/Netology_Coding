d289ae587320efb1bc201efd192045d4346c0051
polygalova <polygalova@yandex-team.ru>
2018-01-30 10:45:35 +0300

    translation

--- homework/3.2-requests/practice.py
+++ homework/3.2-requests/practice.py
@@ -1,32 +1,33 @@
 import requests
+import chardet
+
 
 def translate_it(text):
-    """
-    YANDEX translation plugin
-
-    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
-
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-
-    :param text: <str> text for translation.
-    :return: <str> translated text.
-    """
+    lang_from = input('Исходный язык ')
+    lang_to = input('Конечный язык ')
+    if lang_to == '':
+        lang_to = 'ru'
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
-
     params = {
         'key': key,
-        'lang': 'ru-en',
+        'lang': lang_from + '-' + lang_to,
         'text': text,
     }
     response = requests.get(url, params=params).json()
     return ' '.join(response.get('text', []))
 
-a = translate_it('Привет')
-print(a)
+
+def get_text_write_result():
+    path_from = input('Введите путь к файлу с текстом ')
+    path_to = input('Введите путь к файлу с результатом ')
+    with open(path_from, 'rb') as f:
+        text_in = f.read()
+        code_in = chardet.detect(text_in)
+        text_in_decoded = text_in.decode(code_in['encoding'])
+    text_out = translate_it(text_in_decoded)
+    with open(path_to, 'w', encoding='utf8') as f:
+        f.write(text_out)
+
+
+get_text_write_result()
